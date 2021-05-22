328d7cd4d47fd760f74c523bc0e5802a952d789b
Alexander Plotnikov <lotos_toi@mail.ru>
2021-05-22 10:21:59 +1000

    added alias in the vue.config.js

--- project/vue.config.js
+++ project/vue.config.js
@@ -1,23 +1,33 @@
+const path = require('path')
+
 module.exports = {
-    lintOnSave: false,
-    css: {
-      loaderOptions: {
-        scss: {
-          additionalData: `@import "~@/assets/scss/global.scss";`,
-        },
+  lintOnSave: false,
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "~@/assets/scss/global.scss";`,
       },
     },
-    configureWebpack: (config) => {
-      return {
-        devServer: {
-          proxy: {
-            '/rout': {
-              target: 'http://0.0.0.0:3000',
-              secure: false,
-              changeOrigin: true,
-            },
+  },
+  configureWebpack: (config) => {
+    return {
+      devServer: {
+        proxy: {
+          '/rout': {
+            target: 'http://0.0.0.0:3000',
+            secure: false,
+            changeOrigin: true,
           },
         },
-      }
-    },
+      },
+    }
+  },
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@c', path.resolve(__dirname, 'src/components'))
+      .set('@s', path.resolve(__dirname, 'src/store'))
+      .set('@r', path.resolve(__dirname, 'src/router'))
+      .set('@p', path.resolve(__dirname, 'src/views'))
+      .set('@api', path.resolve(__dirname, 'src/api'))
   }
+}
