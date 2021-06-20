07424b2d5e8f2a3fb2ffd23600c69694ba1c6350
Alexander Plotnikov <lotos_toi@mail.ru>
2021-06-20 12:10:39 +1000

    goal name in the add goal is ready

---
+++ dream-box/src/components/app/goals/add-goal/add-goal.vue
@@ -0,0 +1,91 @@
+<template>
+  <form class="add-goal" @submit.prevent="() => {}">
+    <div class="goal-name">
+      <ChooseTheme />
+      <input
+        type="text"
+        name="goal-name"
+        class="input-goal-name"
+        placeholder="Name of the goal"
+      />
+      <button class="menu"></button>
+      <button class="close"></button>
+    </div>
+  </form>
+</template>
+
+<script>
+import ChooseTheme from "@c/app/goals/add-goal/choose-theme";
+export default {
+  components: {
+    ChooseTheme,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.add-goal {
+  @include fc-c-c-b;
+  background-color: $color-base-gray;
+  padding: 12px 16px;
+  border-radius: 8px;
+  margin-bottom: 18px;
+}
+.goal-name {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.input-goal-name {
+  margin: 0 20px;
+  display: flex;
+  flex-grow: 1;
+  width: auto;
+  height: 28px;
+  border-radius: 8px;
+  background-color: $color-base-light;
+  @include o-b-none;
+  padding: 4px 20px;
+  box-sizing: border-box;
+  @include placeholder {
+    font-family: $base-ff;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 20px;
+    letter-spacing: 0.1px;
+    color: #a9acbf;
+  }
+}
+.menu,
+.close {
+  display: block;
+  background-color: #e6e9f8;
+  width: 100%;
+  width: 24px !important;
+  height: 24px !important;
+  border-radius: 50%;
+  margin-left: 10px;
+  @include o-b-none;
+  box-sizing: border-box;
+}
+.menu {
+  background-image: url("~@/assets/images/goals/three-dots.png");
+  background-repeat: no-repeat;
+  background-position: 50% 50%;
+}
+.close {
+  background-image: url("~@/assets/images/goals/arrow-top-gray.png");
+  background-repeat: no-repeat;
+  background-position: 50% 50%;
+}
+.menu:hover,
+.close:hover {
+  background-color: lighten(#e6e9f8, 20%);
+}
+.menu:active,
+.close:active {
+  background-color: lighten($color-base-blue, 40%);
+}
+</style>
---
+++ dream-box/src/components/app/goals/add-goal/choose-theme/choose-theme.vue
@@ -0,0 +1,158 @@
+<template>
+  <div class="wrapper-choose-theme">
+    <button :class="{ active: isOpen }" @click.prevent="toggleOnOff">
+      {{ currentValue }}
+      <div class="wrapper-themes" :class="{ active: isOpen }">
+        <div class="themes" @click.prevent.stop>
+          <span
+            class="theme"
+            v-for="theme in themes"
+            :key="theme"
+            @click.prevent.stop="chooseTheme(theme)"
+            >{{ theme }}</span
+          >
+        </div>
+      </div>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  DEFAULT_TEXT: "Choose theme",
+  MAX_LENGTH: 12,
+  props: {
+    active: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  emits: {
+    "sent-current-value": (payload) => typeof payload === "string",
+  },
+  data() {
+    return {
+      isOpen: false,
+      selectedTheme: null,
+      themes: [
+        "family&friends",
+        "cereer",
+        "self-development",
+        "spiritual",
+        "finance",
+        "sports",
+        "health",
+        "relax",
+      ],
+    };
+  },
+  methods: {
+    toggleOnOff() {
+      this.isOpen = !this.isOpen;
+    },
+    chooseTheme(theme) {
+      this.selectedTheme =
+        theme.length > this.$options.MAX_LENGTH
+          ? theme.slice(0, this.$options.MAX_LENGTH) + "..."
+          : theme;
+      this.$emit("sent-current-value", theme);
+    },
+  },
+  computed: {
+    currentValue() {
+      return this.selectedTheme
+        ? this.selectedTheme
+        : this.$options.DEFAULT_TEXT;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+$anim-time: 0.3s;
+$radius: 13px;
+
+button {
+  @include fr-s-c-b;
+  @include o-b-none;
+  padding: 0 12px;
+  width: 136px;
+  height: 28px;
+  background: $color-base-blue;
+  border: 1px solid $color-base-blue;
+  box-sizing: border-box;
+  border-radius: $radius;
+  font-family: $base-ff;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 20px;
+  letter-spacing: 0.1px;
+  color: $color-font-light;
+  position: relative;
+  transition: border-radius $anim-time;
+  z-index: 150;
+}
+button.active {
+  border-radius: $radius $radius 0 0;
+}
+button:after {
+  content: "";
+  width: $radius;
+  height: 10px;
+  position: absolute;
+  background: url("~@/assets/images/goals/arrow-bot.png") no-repeat;
+  background-size: 80%;
+  background-position-x: 50%;
+  background-position-y: 50%;
+  right: 8px;
+  transition: transform $anim-time;
+}
+button.active:after {
+  transform: rotate(180deg);
+}
+.wrapper-themes {
+  width: 136px;
+  @include fc-c-c-b;
+  padding-top: 15px;
+  position: absolute;
+  left: 0;
+  top: calc(100% - 15px);
+  border-radius: 0 0 $radius $radius;
+  border: 2px solid $color-base-blue;
+  border-top: none;
+  transform-origin: 50% 0;
+  opacity: 0;
+  transition: transform $anim-time, opacity 0.1s;
+  transform: scaleY(0);
+}
+
+.themes {
+  padding-top: 5px;
+  border-radius: 0 0 $radius $radius;
+  cursor: auto;
+  @include fc-c-c-b;
+  background-color: $color-base-light;
+  width: 100%;
+  z-index: 120;
+  & > span {
+    cursor: pointer;
+    font-family: $base-ff;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 14px;
+    line-height: 20px;
+    letter-spacing: 0.1px;
+    color: $color-font-dark;
+    margin: 5px 0;
+    width: 100%;
+    padding: 5px;
+  }
+  & > span:hover {
+    color: $color-base-blue;
+  }
+}
+.wrapper-themes.active {
+  transform: scaleY(1);
+  opacity: 1;
+}
+</style>
