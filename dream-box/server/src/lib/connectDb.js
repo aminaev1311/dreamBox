d910359d5b1e1d509446a5c7fb7d5f044e7f829c
Alexander Plotnikov <lotos_toi@mail.ru>
2021-07-11 00:21:48 +1000

    fix bug with autologin

--- dream-box/server/src/lib/connectDb.js
+++ dream-box/server/src/lib/connectDb.js
@@ -3,8 +3,10 @@ mongoose.set("useFindAndModify", false);
 
 module.exports = async function ({ login, password, nameDataBase }, cb) {
   try {
-    const link = `mongodb://127.0.0.1:27017/${nameDataBase}?authSource=admin`;
-    //const link = `mongodb://${login}:${password}@cluster0.n6oze.mongodb.net/${nameDataBase}?retryWrites=true&w=majority`;
+    const link =
+      process.env.NODE_ENV !== "development"
+        ? `mongodb://127.0.0.1:27017/${nameDataBase}?authSource=admin`
+        : `mongodb+srv://${login}:${password}@cluster0.n6oze.mongodb.net/${nameDataBase}?retryWrites=true&w=majority`;
     const db = await mongoose.connect(link, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log("Connection to db is success...");
     cb(db);
